b33b2e566f9f87b2fe44d7f6e72f873965e3bf0b
denos <costelasdenis@gmail.com>
2023-11-20 15:21:53 +0200

    tema_lab_4

--- src/main/java/teme/teme_lab_4.java
+++ src/main/java/teme/teme_lab_4.java
@@ -1,2 +1,19 @@
-package teme;public class teme_lab_4 {
+package teme;
+
+public class teme_lab_4 {
+
+    public static void main(String[] args) {
+        System.out.println(returnMiddleChars("masina"));
+    }
+
+    public static String returnMiddleChars(String word){
+        final int wordLength = word.length();
+        String returnString = "";
+        if(wordLength%2 == 0){
+            returnString = returnString + word.charAt(wordLength/2-1) + word.charAt(wordLength/2);
+        }else{
+            returnString = returnString + word.charAt(wordLength/2);
+        }
+        return returnString;
+    }
 }
